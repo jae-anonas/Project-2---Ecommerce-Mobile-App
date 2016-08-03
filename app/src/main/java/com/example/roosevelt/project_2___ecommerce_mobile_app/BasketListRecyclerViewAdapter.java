7412188a08245959013093cc4fc07cc1f258327e
Jae Anonas <jae.anonas@gmail.com>
2016-08-03 11:03:35 -0400

    add async method in main for adding food records in database

--- app/src/main/java/com/example/roosevelt/project_2___ecommerce_mobile_app/BasketListRecyclerViewAdapter.java
+++ app/src/main/java/com/example/roosevelt/project_2___ecommerce_mobile_app/BasketListRecyclerViewAdapter.java
@@ -2,12 +2,14 @@ package com.example.roosevelt.project_2___ecommerce_mobile_app;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.support.annotation.Dimension;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import java.util.List;
 import java.util.Locale;
@@ -19,7 +21,7 @@ public class BasketListRecyclerViewAdapter extends RecyclerView.Adapter<BasketLi
 
     private Context mContext;
     private List<FoodInBasket> mFoodItemList;
-    FoodDBHelper dbHelper;
+//    FoodDBHelper dbHelper;
     private OnChangeQuantityListener mListener;
 
     public void setFoodItemList(List<FoodInBasket> mFoodItemList) {
@@ -41,7 +43,7 @@ public class BasketListRecyclerViewAdapter extends RecyclerView.Adapter<BasketLi
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.basket_item_layout, parent, false);
         BasketListCardHolder viewHolder = new BasketListCardHolder(view);
-        dbHelper = FoodDBHelper.getInstance(parent.getContext());
+//        dbHelper = FoodDBHelper.getInstance(parent.getContext());
         return viewHolder;
     }
 
--- app/src/main/java/com/example/roosevelt/project_2___ecommerce_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/roosevelt/project_2___ecommerce_mobile_app/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,12 +18,18 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     FoodDBHelper mFoodDBHelper;
+    private Cursor cursor;
+    private ProgressBar mProgressbar;
 
 
     @Override
@@ -31,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        insertFoodData();
+        mProgressbar = (ProgressBar) findViewById(R.id.progressbar);
+
+        insertFoodDataAsync();
 
         Intent intent = new Intent(Intent.ACTION_SEARCH);
         intent.putExtra(SearchManager.QUERY, "");
@@ -41,8 +50,6 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                if(Constants.user == null)
-//                    insertFoodData();
 
                 Intent intent = new Intent(MainActivity.this, BasketActivity.class);
                 startActivity(intent);
@@ -94,7 +101,9 @@ public class MainActivity extends AppCompatActivity {
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
             mRecyclerView.setLayoutManager(linearLayoutManager);
 
-            Cursor cursor = mFoodDBHelper.getFoodBySearch(query);
+            //TODO run the next line on a separate thread
+//            getFoodBySearchAsync(query);
+            cursor = mFoodDBHelper.getFoodBySearch(query);
 //            Cursor cursor = mFoodDBHelper.getAllFoodBasicInfo();
 
             if (!cursor.moveToFirst()){
@@ -105,6 +114,236 @@ public class MainActivity extends AppCompatActivity {
             mRecyclerView.setAdapter(foodListCursorAdapter);
         }
     }
+
+    private void insertFoodDataAsync(){
+        Food food1 = new Food(
+                1,
+                500,
+                R.drawable.bacon_burger,
+                "Bacon and Cheese Burger",
+                "not your regular burger, with cheese and bacon plus fries on the side",
+                "sandwich",
+                " burger sandwich ",
+                20);
+
+        Food food2 = new Food(
+                2,
+                500,
+                R.drawable.bread_basket,
+                "Basket of Bread",
+                "an old-school basket of freshly-baked bread",
+                "bread",
+                " bread sandwich ",
+                7);
+
+        Food food3 = new Food(
+                3,
+                500,
+                R.drawable.bread_food_salad_sandwich,
+                "Veggie Sandwich",
+                "not your regular sandwich meal, with alfalfa sprouts and pomelo juice on the side",
+                "sandwich",
+                " sandwich vegetarian vegan bread ",
+                10);
+
+        Food food4 = new Food(
+                4,
+                500,
+                R.drawable.broccoli_sandwich,
+                "Broccoli Sandwich",
+                "broccoli, tomatoes and cucumbers on a sesame seed bun",
+                "sandwich",
+                " vegan vegetarian sandwich ",
+                10);
+
+        Food food5 = new Food(
+                5,
+                500,
+                R.drawable.bunch_of_carrots,
+                "Bunch of Carrots",
+                "just a bunch of carrots with one white and one red onion on the side",
+                "vegetable",
+                " vegan vegetarian vegetables ",
+                5);
+
+        Food food6 = new Food(
+                6,
+                500,
+                R.drawable.bunch_of_tomatoes,
+                "Bunch of Tomatoes",
+                "a bunch of six Campari tomatoes",
+                "vegetable",
+                " vegan vegetarian sandwich ",
+                5);
+
+        Food food7 = new Food(
+                7,
+                500,
+                R.drawable.burger_with_fries,
+                "Hamburger with Potato Wedges",
+                "100% Angus beef burger with Spinach and onions on a sesame seed bun",
+                "sandwich",
+                " burger sandwich potato potatoes ",
+                12);
+
+        Food food8 = new Food(
+                8,
+                500,
+                R.drawable.food_colorful_dessert_sweet,
+                "Chocolate Cupcakes",
+                "12 chocolate cupcakes on a tray with assorted sprinkles",
+                "dessert",
+                " dessert sweets cupcakes chocolate ",
+                18);
+
+        Food food9 = new Food(
+                9,
+                500,
+                R.drawable.food_dinner_grilled_shashlik,
+                "Shashlik",
+                "3 skewers of shashlik goodness, with onions and capsicum",
+                "grilled",
+                " skewer vegetarian ",
+                10);
+
+        Food food10 = new Food(
+                10,
+                500,
+                R.drawable.food_pasta_tomato,
+                "Tomato Linguine",
+                "a gorgeous plate of linguine with chunky tomato sauce and olives",
+                "pasta",
+                " pasta tomato linguine ",
+                12);
+
+        Food food11 = new Food(
+                11,
+                500,
+                R.drawable.meat_vegetables_gemuesepiess_mushrooms_111131,
+                "Gemuesepiess",
+                "3 Skewers of vegetable goodness",
+                "grilled",
+                " vegetarian vegan cucumber portobello mushroom ",
+                10);
+
+        Food food12 = new Food(
+                12,
+                500,
+                R.drawable.steak_juicy,
+                "12oz Steak",
+                "12oz Angus Steak cooked to your liking, served with boiled and seasoned marble potatoes",
+                "sandwich",
+                " meat steak angus 12oz ",
+                10);
+
+        final List<Food> foods = new ArrayList<>(12);
+        foods.add(food1);
+        foods.add(food2);
+        foods.add(food3);
+        foods.add(food4);
+        foods.add(food5);
+        foods.add(food6);
+        foods.add(food7);
+        foods.add(food8);
+        foods.add(food9);
+        foods.add(food10);
+        foods.add(food11);
+        foods.add(food12);
+
+        final FoodDBHelper foodDBHelper = FoodDBHelper.getInstance(this);
+
+
+        foodDBHelper.newFood(food1);
+
+
+
+        AsyncTask<Void, Integer, Void> task = new AsyncTask<Void, Integer, Void>() {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                mProgressbar.setMax(12);
+                mProgressbar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                for(int i = 0; i < 12; i++){
+                    foodDBHelper.newFood(foods.get(i));
+                    publishProgress(i + 1);
+                }
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+                mProgressbar.setProgress(values[0]);
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                mProgressbar.setVisibility(View.INVISIBLE);
+            }
+        };
+
+
+        User user = new User(1,"jaeanonas", "1234");
+
+        Constants.user = user;
+
+        foodDBHelper.newUser(Constants.user);
+
+        task.execute();
+    }
+
+    /**
+     *
+     *
+     private void addDatabaseItemsAsync(){
+
+     task = new AsyncTask<Void, Integer, Cursor>() {
+
+    @Override
+    protected void onPreExecute() {
+    super.onPreExecute();
+    mProgressBar.setVisibility(View.VISIBLE);
+    mProgressBar.setMax(3000);
+    }
+
+    @Override
+    protected Cursor doInBackground(Void... params) {
+    for (int i = 0; i < 3000; i++) {
+    ExampleDBHelper.getInstance(getApplicationContext()).addName("John","Doe");
+    publishProgress(i + 1);
+    }
+
+    return ExampleDBHelper.getInstance(getApplicationContext()).getFirstHundred();
+    }
+
+    @Override
+    protected void onPostExecute(Cursor cursor) {
+    super.onPostExecute(cursor);
+    int count = ExampleDBHelper.getInstance(getApplicationContext()).getItemCount();
+    mProgressBar.setVisibility(View.INVISIBLE);
+    mTextView.setText("All items have been added! Current item count " + count);
+
+    mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+    super.onProgressUpdate(values);
+    mTextView.setText("Adding items to database...." + values[0]);
+    mProgressBar.setProgress(values[0]);
+
+    }
+    };
+
+     task.execute();
+     }
+     *
+     */
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
